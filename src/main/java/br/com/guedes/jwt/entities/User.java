439d7efdea9724436f128364f087a938a5a8c81b
Gabriela <zooiv3rde@gmail.com>
2019-09-04 22:20:13 -0300

    profiles created

--- src/main/java/br/com/guedes/jwt/entities/User.java
+++ src/main/java/br/com/guedes/jwt/entities/User.java
@@ -1,8 +1,13 @@
 package br.com.guedes.jwt.entities;
 
+import br.com.guedes.jwt.entities.enums.Profile;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Document
 public class User {
@@ -14,12 +19,17 @@ public class User {
     @JsonIgnore
     private String password;
 
-    public User() {}
+    private Set<Integer> profiles = new HashSet<>();
+
+    public User() {
+        addProfile(Profile.USER);
+    }
 
     public User(String id, String name, String password) {
         this.id = id;
         this.name = name;
         this.password = password;
+        addProfile(Profile.USER);
     }
 
     public String getId() {
@@ -32,5 +42,13 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    public Set<Profile> getProfile() {
+        return profiles.stream().map(x -> Profile.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void addProfile(Profile profile) {
+        profiles.add(profile.getCod());
     }
 }
