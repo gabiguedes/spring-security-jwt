88a80fa82b85420452cb3d146ccee3ffea6ef083
Gabriela <zooiv3rde@gmail.com>
2019-09-08 18:50:51 -0300

    add endpoint users

--- src/main/java/br/com/guedes/jwt/resources/UserResource.java
+++ src/main/java/br/com/guedes/jwt/resources/UserResource.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping(value = "/users")
 public class UserResource {
 
     @Autowired
