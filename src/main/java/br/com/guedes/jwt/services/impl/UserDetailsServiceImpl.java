ddd1f19e3665cbafd2856bb570d7754a27f81540
Gabriela <zooiv3rde@gmail.com>
2019-09-08 19:15:55 -0300

    create load user by user name

--- src/main/java/br/com/guedes/jwt/services/impl/UserDetailsServiceImpl.java
+++ src/main/java/br/com/guedes/jwt/services/impl/UserDetailsServiceImpl.java
@@ -1,13 +1,25 @@
 package br.com.guedes.jwt.services.impl;
 
+import br.com.guedes.jwt.entities.User;
+import br.com.guedes.jwt.repositories.UserRepository;
+import br.com.guedes.jwt.security.UserSS;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findByEmail(email);
+
+        if(user == null) {
+            throw new UsernameNotFoundException(email);
+        }
+        return new UserSS();
     }
 }
