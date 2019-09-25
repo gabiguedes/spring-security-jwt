d5b193f9baa553752020e68157dd7674c4bffc3d
Gabriela <zooiv3rde@gmail.com>
2019-09-24 22:55:46 -0300

    create new vector for method post

--- src/main/java/br/com/guedes/jwt/config/SecurityConfig.java
+++ src/main/java/br/com/guedes/jwt/config/SecurityConfig.java
@@ -39,11 +39,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/championships/**"
     };
 
+    private static final String[] PUBLIC_MATCHERS_POST = {
+            "/users/**"
+    };
+
     @Override
    protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable();
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+                .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET)
+                .permitAll()
+                .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST)
+                .permitAll()
                 .antMatchers(PUBLIC_MATCHERS)
                 .permitAll()
                 .anyRequest()
